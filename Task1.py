dd2ebba2213f6b214ae38843314a9daf2368cc61
Mohammad Mehdi Rastikerdar <128183766+mmehdirk@users.noreply.github.com>
2023-12-13 00:08:46 -0500

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,14 +1,11 @@
+import numpy as np
+import random
 import torch
 from tqdm import tqdm
 from transformers import AutoTokenizer, AutoModel, AutoModelForSeq2SeqLM
 import argparse
 import pickle
 
-parser = argparse.ArgumentParser('places_c_r')
-parser.add_argument('-start', type=int)           # positional argument
-parser.add_argument('-end', type=int)      # option that takes a value
-parser.add_argument('-idx', type=str)  # on/off flag
-args = parser.parse_args()
 
 tokenizer = AutoTokenizer.from_pretrained('facebook/contriever-msmarco')
 model = AutoModel.from_pretrained('facebook/contriever-msmarco')
@@ -47,7 +44,7 @@ def mean_pooling(token_embeddings, mask):
 
 
 score_p = {}
-for input_num in range(args.start, args.end):
+for input_num in range(len(data)):
     score_p.update({data[input_num]['id']:[]})
     print(input_num)
     query = phi_q[input_num]
@@ -63,7 +60,48 @@ for input_num in range(args.start, args.end):
       score_p[data[input_num]['id']].append(rel_score.item())
         
         
-import pickle 
+model = AutoModelForSeq2SeqLM.from_pretrained("google/flan-t5-xxl")
+tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-xxl")
+# device = "cuda:0" if torch.cuda.is_available() else "cpu"
+# model = model.to(device)
 
-with open("/work/mrastikerdar_umass_edu/ir_project/scores_task1_train/score_p_{idx}.pkl".format(idx = args.idx),"wb") as f:
-    pickle.dump(score_p, f)
+#### Contriever Top-K ####
+
+import re
+
+    
+score_list = list(score_p.values())
+ids = list(score_p.keys())
+
+k = 4
+prompts = []
+for user_num in range(len(data)):
+    sorted_score_user_idx = np.flip(np.argsort(score_list[user_num]))
+    prompt = {}
+    prompt['id'] = data[user_num]['id']
+    prompt['text'] = ''
+    ppep_titles = ''
+    for i in range(1,k+1):
+        retrieved_idx = sorted_score_user_idx[i]
+        ppep_titles += data[user_num]['profile'][retrieved_idx]['title']        
+        ppep_titles += ', and '    
+    original_input = data[user_num]['input']
+    original_title = re.findall(r'\"(.*?)\"', original_input)[0]
+    augmented_title = ppep_titles + original_title 
+    first_quote = original_input.find("\"")
+    second_quote = original_input.find("\"", first_quote + 1)
+    prompt['text'] = original_input[:first_quote + 1] + augmented_title + original_input[second_quote:]
+    prompts.append(prompt)
+
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+
+prediction_task1 = []
+for i in range(len(data)):
+    
+    print(i)
+    inputs = tokenizer(prompts[i]['text'], return_tensors="pt").to(device)
+    model = model.to(device)
+    outputs = model.generate(**inputs)
+    prediction_task1.append(tokenizer.batch_decode(outputs, skip_special_tokens=True)[0])
+    
